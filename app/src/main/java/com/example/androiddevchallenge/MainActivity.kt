4df5b859ddc3c66cba3735f261caa4d1ea21e33b
Pankaj <pankaj.rai16@gmail.com>
2021-03-08 12:08:57 +0530

    Added red theme

--- app/src/main/java/com/example/androiddevchallenge/MainActivity.kt
+++ app/src/main/java/com/example/androiddevchallenge/MainActivity.kt
@@ -18,44 +18,152 @@ package com.example.androiddevchallenge
 import android.os.Bundle
 import androidx.activity.compose.setContent
 import androidx.appcompat.app.AppCompatActivity
+import androidx.compose.animation.animateColorAsState
+import androidx.compose.animation.core.*
+import androidx.compose.foundation.Canvas
+import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.drawscope.translate
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.androiddevchallenge.ui.theme.MyTheme
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.isActive
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             MyTheme {
-                MyApp()
+                // A surface container using the 'background' color from the theme
+                Surface(color = Color(251,251,251)) {
+                    Box(Modifier.fillMaxSize(), contentAlignment = Alignment.Center) {
+                        var startCountDownTimer by remember { mutableStateOf(false) }
+                        var countdownTimer by remember { mutableStateOf(100) }
+
+                        if (startCountDownTimer) {
+                            LaunchedEffect(key1 = false) {
+                                while (isActive && countdownTimer > 0) {
+                                    delay(1000)
+                                    countdownTimer -= 1
+                                }
+                            }
+                        }
+
+                        Box(
+                            modifier = Modifier
+                                .size(300.dp)
+                                .border(6.dp, color = Color.Red, CircleShape)
+                                .clip(CircleShape)
+
+                                .clickable {
+                                    startCountDownTimer = startCountDownTimer.not()
+
+                                    if (countdownTimer == 0) {
+                                        countdownTimer = 100
+                                    }
+                                },
+                            contentAlignment = Alignment.Center
+                        ) {
+
+                            WaterWave(countdownTimer, startCountDownTimer)
+                            Timer(countdownTimer, startCountDownTimer)
+                        }
+                    }
+                }
             }
         }
     }
 }
 
-// Start building your app here!
 @Composable
-fun MyApp() {
-    Surface(color = MaterialTheme.colors.background) {
-        Text(text = "Ready... Set... GO!")
+fun Timer(countdownTimer: Int, timerStarted: Boolean) {
+    val fontColor by animateColorAsState(
+        targetValue = when (countdownTimer) {
+            in 60..100 -> Color.White
+            in 20..59 -> Color.Black
+            else -> Color.Red
+        }
+    )
+
+    Text(
+        text = getStatus(countdownTimer, timerStarted),
+        style = TextStyle(
+            color = fontColor,
+            fontWeight = FontWeight.ExtraBold, fontSize = 82.sp
+        )
+    )
+}
+
+private fun getStatus(countdownTimer: Int, timerStarted: Boolean): String {
+    return when {
+        countdownTimer == 0 || countdownTimer == 100 -> "Start"
+        timerStarted.not() -> "Pause"
+        else -> countdownTimer.toString()
     }
 }
 
-@Preview("Light Theme", widthDp = 360, heightDp = 640)
 @Composable
-fun LightPreview() {
-    MyTheme {
-        MyApp()
-    }
-}
+fun WaterWave(countdownTimer: Int, timerStarted: Boolean) {
 
-@Preview("Dark Theme", widthDp = 360, heightDp = 640)
-@Composable
-fun DarkPreview() {
-    MyTheme(darkTheme = true) {
-        MyApp()
+    val waveWidth = 250
+    val path = androidx.compose.ui.graphics.Path()
+    val originalY by animateIntAsState(
+        targetValue =
+        if (countdownTimer == 0) 300 else Math.abs(countdownTimer.times(3) - 300)
+    )
+
+    val waveHump by animateFloatAsState(targetValue = if (timerStarted) 40f else 0f)
+
+    val deltaXAnim = rememberInfiniteTransition()
+    val dx by deltaXAnim.animateFloat(
+        initialValue = 0f,
+        targetValue = 1f,
+        animationSpec = infiniteRepeatable(
+            animation = tween(500, easing = LinearEasing)
+        )
+    )
+
+    Canvas(modifier = Modifier.fillMaxSize()) {
+        translate {
+            drawPath(path = path, Color.Red)
+            path.reset()
+
+            val halfWaveWidth = waveWidth / 2
+            path.moveTo(-waveWidth + (waveWidth * dx), originalY.dp.toPx())
+
+            for (i in -waveWidth..(size.width.toInt() + waveWidth) step waveWidth) {
+                path.relativeQuadraticBezierTo(
+                    20f,
+                    waveHump,
+                    halfWaveWidth.toFloat(),
+                    0f
+                )
+                path.relativeQuadraticBezierTo(
+                    halfWaveWidth.toFloat() / 2,
+                    waveHump,
+                    halfWaveWidth.toFloat(),
+                    0f
+                )
+            }
+
+            path.lineTo(size.width, size.height)
+            path.lineTo(0f, size.height)
+            path.close()
+        }
     }
 }
